fbb8b5901401076d644a428811152ba329d20062
haroon01 <haroon815@gmail.com>
2020-02-15 17:46:18 +0000

    Completed week 16 - made all classes

---
+++ week 16/src/StudentAccount.java
@@ -0,0 +1,54 @@
+public class StudentAccount {
+    private String name;
+    private String accountNumber;
+    private String sortCode;
+    private Double balance;
+    private static Double interest;
+
+    public StudentAccount(String name, String accountNumber, String sortCode, Double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.sortCode = sortCode;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getSortCode() {
+        return sortCode;
+    }
+
+    public void setSortCode(String sortCode) {
+        this.sortCode = sortCode;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public static Double getInterest() {
+        return interest;
+    }
+
+    public static void setInterest(Double interest) {
+        StudentAccount.interest = interest;
+    }
+}
--- week 16/src/main.java
+++ week 16/src/main.java
@@ -5,14 +5,34 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         //   Scanner input
-        Locale locale = new Locale("en", "GB");
-        NumberFormat cf = NumberFormat.getCurrencyInstance(locale);
-        CurrentAccount ca = new CurrentAccount("Haroon","4829384","48-37-28",0.0);
-        System.out.println("Your Current Balance is: "+cf.format(ca.getBalance()));
+        //Locale locale = new Locale("en", "GB");
+        //NumberFormat cf = NumberFormat.getCurrencyInstance(locale);
+        CurrentAccount ca = new CurrentAccount("Haroon","4829384","48-37-28",0.0, false);
+        System.out.println("Your Current Balance is: "+ca.getBalance());
+        System.out.println("\nHi, what would you like to withdraw or deposit? ");
+
+
         Scanner input = new Scanner(System.in);
-        System.out.println("how much would you like to deposit? ");
-        Double amount = input.nextDouble();
-        ca.deposit(amount);
+        String usrinput = input.nextLine();
+        if(usrinput.equals("withdraw")){
+            System.out.print("how much would you like to withdraw? ");
+            Double amount = input.nextDouble();
+            ca.withdraw(amount);
+            System.out.println("Your new balance is:"+ca.getBalance());
+        }
+        else if(usrinput.equals("deposit")){
+            System.out.print("How much would you like to deposit? ");
+            Double amount = input.nextDouble();
+            ca.deposit(amount);
+            System.out.println("Your new balance is: "+ca.getBalance());
+        }
+        else{
+            System.out.println("errror");
+        }
+
+
+
+
 
     }
 }
