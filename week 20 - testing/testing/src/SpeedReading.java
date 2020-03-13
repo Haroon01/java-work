d7698a12bb650f014986d1ed0a2bfbf180daaef4
haroon01 <haroon815@gmail.com>
2020-03-13 15:04:13 +0000

    added SpeedReading Test class

---
+++ week 20 - testing/testing/src/SpeedReading.java
@@ -0,0 +1,12 @@
+public class SpeedReading {
+    private String speed;
+
+
+    public SpeedReading(String speed) {
+        this.speed = speed;
+    }
+
+    private String parseSpeedReading(){
+
+    }
+}
