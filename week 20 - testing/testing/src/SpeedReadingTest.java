6edb9b423bae2d11e876a217ba7303ff68a175b9
haroon01 <haroon815@gmail.com>
2020-03-13 14:15:33 +0000

    week 20 added

---
+++ week 20 - testing/testing/src/SpeedReadingTest.java
@@ -0,0 +1,25 @@
+package testing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpeedReadingTest {
+    private SpeedReading sr;
+    @Before
+    public void setUp() throws Exception {
+        sr = new SpeedReading();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void parseSpeedReading() {
+        assertEquals(sr.parseSpeedReading("H55"), 55);
+    }
+}
---
+++ week 20 - testing/testing/src/Sum.java
@@ -0,0 +1,19 @@
+package testing;
+
+public class Sum {
+
+    public Sum() {
+    }
+
+    public int add(int x, int y){
+        return x + y;
+    }
+
+    public int times(int x, int y){
+        return x * y;
+    }
+
+    public int sub(int x, int y){
+        return x - y;
+    }
+}
